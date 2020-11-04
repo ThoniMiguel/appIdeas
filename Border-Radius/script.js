3f401d226cafba8f4d89ab0a40faf5b0db636063
ThoniDesktop <wanok8000@outlook.com>
2020-11-04 02:11:25 -0300

    done with border-radius

--- Border-Radius/script.js
+++ Border-Radius/script.js
@@ -5,26 +5,26 @@ const topLeft = document.querySelector(".border1");
 const topRight = document.querySelector(".border2");
 const botLeft = document.querySelector(".border3");
 const botRight = document.querySelector(".border4");
-let topLVal = [];
-let topRVal = [];
-let botLVal = [];
-let botRVal = [];
+const myBox = document.querySelector(".box");
+const unit = "px";
+
+copyBtn.addEventListener("click", () => {
+  copyArea.value = `{\nborder-top-left-radius:${topLeft.value}${unit};\nborder-top-right-radius:${topRight.value}${unit};\nborder-bottom-left-radius:${botLeft.value}${unit};\nborder-bottom-right-radius:${botRight.value}${unit};\n}`;
+});
 
 topLeft.addEventListener("input", (e) => {
-    if(!e.data){
-        return
-    }
-    topLVal.push(e.data);
-    let temp = topLVal.join("");
-    console.log(temp);
+  let borderValue = topLeft.value;
+  myBox.style.borderTopLeftRadius = `${borderValue}${unit}`;
 });
-topRight.addEventListener("input", (e) => {});
-botLeft.addEventListener("input", (e) => {});
-botRight.addEventListener("input", (e) => {});
-
-function writesOnTextArea() {
-  return `            border-top-left-radius: 
-            border-top-right-radius:
-            border-bottom-left-radius:
-            border-bottom-right-radius:`;
-}
+topRight.addEventListener("input", (e) => {
+  let borderValue = topRight.value;
+  myBox.style.borderTopRightRadius = `${borderValue}${unit}`;
+});
+botLeft.addEventListener("input", (e) => {
+  let borderValue = botLeft.value;
+  myBox.style.borderBottomLeftRadius = `${borderValue}${unit}`;
+});
+botRight.addEventListener("input", (e) => {
+  let borderValue = botRight.value;
+  myBox.style.borderBottomRightRadius = `${borderValue}${unit}`;
+});
